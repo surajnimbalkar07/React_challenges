757a0e7915b32b8bc59439fbd95cfc1d07c7cb8c
surajnimbalkar07 <136218136+surajnimbalkar07@users.noreply.github.com>
2024-07-09 22:16:59 +0530

    initial commit

---
+++ challenge1/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(10);
+
+ const me= useEffect(() => {
+    const interval = setInterval(() => {
+      setNum((currentNum) => {
+        if (currentNum > 0) {
+          return currentNum - 1;
+        } else {
+
+          // Stop the interval when num reaches 0
+          clearInterval(interval);//used closure concept here, get access of parent variable
+          return num;
+        }
+      });
+    }, 1000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+  
+  const handleSubmit = () => {
+    setCount((count) => count + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <p>time left: {num}</p>
+      <button onClick={handleSubmit}>+</button>
+    </div>
+  );
+}
---
+++ challenge1/src/README.md
@@ -0,0 +1,3 @@
+## problem statement
+
+Create a React component that includes a counter (count) initialized to 0 and a timer (num) initialized to 10. Implement functionality so that clicking a button increments the counter (count) until the timer (num) reaches 0. The timer should decrement every second, and when it reaches 0, clicking the button should no longer increment the counter.
